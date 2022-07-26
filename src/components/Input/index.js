17f92a5c72d02b82e73d8721e04e2d5a09a6fac6
Suraj Yadav <ysuraj877@gmail.com>
2022-07-26 22:42:43 +0530

    added optional text

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,6 +1,9 @@
 export default function Input({label, ...props}){
     return <div className="m0 flex flex-column">
-        <label htmlFor={props?.name}>{props?.name}</label>
+        <div className="m0">
+            <label htmlFor={props?.name}>{props?.name}</label>
+            <span className="gray">{props?.info}</span>
+        </div>
         <input {...props}/>
     </div>
 }
