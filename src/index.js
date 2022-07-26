67b5a1bab7cc376f840e3f7ff1f6d78021487f75
Suraj Yadav <ysuraj877@gmail.com>
2022-07-26 22:22:56 +0530

    added tachyon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "tachyons/css/tachyons.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
