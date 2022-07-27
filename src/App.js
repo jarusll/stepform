9528779ffce9af34509ffd9ee89670ab6a9860db
Suraj Yadav <ysuraj877@gmail.com>
2022-07-28 00:22:39 +0530

    added logo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import SuccessPage from "./components/SuccessPage";
 function App() {
   return (
     <div className="mw7 center sans-serif near-black">
+      <div className="flex justify-center mt3 items-center" >
+        <img src="/eden.png" className="w2 h2 mr2"/>
+        <h2 className="f3">Eden</h2>
+      </div>
       <Stepform>
         <UserForm />
         <WorkspaceForm />
